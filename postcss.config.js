e6b3f5f0650bb3f9ce35c06d58d3f63ed3e5e6be
Alex Cooper <616126+bondarev@users.noreply.github.com>
2023-07-02 10:38:42 +0400

    Add more readable breakpoints with variables and mixins

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,9 @@ module.exports = {
   map: { inline: true },
   plugins: [
     require('postcss-import'),
+    require('postcss-simple-vars'),
+    require('postcss-map-get'),
+    require('postcss-mixins'),
     require('postcss-nested'),
     require('autoprefixer'),
     require('cssnano'),
